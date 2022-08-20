4b450f0827765611aacab2be5c65333b7f418c06
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-20 13:03:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import simpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import debounce from "lodash.debounce";
 
+const form = document.querySelector('.search-form');
+const input = document.querySelector('input');
+const searchBtn = document.querySelector(`button[type="submit"]`);
+
+
+
+
+console.log(searchBtn);
 
