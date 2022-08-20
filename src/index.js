f694b790dd7764144db65820eae8d19baf61d13a
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-20 15:35:43 +0200

    problem catch error

--- src/index.js
+++ src/index.js
@@ -9,14 +9,34 @@ const searchBtn = document.querySelector(`button[type="submit"]`);
 const API_KEY = '29396920-d4426056c3f6851287cd3980f';
 const perPage = 40;
 
-const renderFirstSearch = async () => {
-    const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=1`);
-    const photos = await response.json();
-    console.log(photos);
+const fetchPhotos = () => {
+    return fetch(
+    `https://pixabay.com/api/?key=${API_KEY}&q=qweqweq&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=1`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      return response.json();
+    }
+  });
 };
 
+const renderFirstSearch = async () => {
+    try {
+        const photos = await fetchPhotos();
+        
+        console.log(photos.hits);
+
+    } catch (error) {
+        console.log('ups');
+    };
+};
+
+// const totalHits = photos.totalHits;
+    // const webformatURL = photos.hits.map(photo => {return photo.webformatURL});
+    // console.log(photos);
+    // console.log(webformatURL);
 // https://pixabay.com/api/?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=1
 
-renderFirstSearch();
-console.log(searchBtn);
+console.log(renderFirstSearch());
 
