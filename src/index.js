ea28c6db1a9c1fd43d49c5ac4ea54101e3bf18dc
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-21 14:41:14 +0200

    problem fetch fixed

--- src/index.js
+++ src/index.js
@@ -9,28 +9,30 @@ const searchBtn = document.querySelector(`button[type="submit"]`);
 const API_KEY = '29396920-d4426056c3f6851287cd3980f';
 const perPage = 40;
 
-const fetchPhotos = async (search) => {
-    const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=1`);
+const fetchPhotos = async (search, pagenr) => {
+    const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&pretty=true&per_page=${perPage}&page=${pagenr}`);
   const photos = await response.json();
   return photos;
 };
 
-form.addEventListener('submit', async (event) => {
-    try {
+form.addEventListener('submit', async event => {
+  try {
     event.preventDefault();
-    const { searchBtn, searchQuery } = event.currentTarget;
-  let trimInput = searchQuery.value.trim();
-        const photos = await fetchPhotos(trimInput);
-      
-      
-    
-    console.log(photos.hits);
-    console.log(trimInput);
+    const { searchBtn, searchQuery } = event.currentTarget;    
+    let trimInput = searchQuery.value.trim();
+    let page = 1;
+    const varPhotos = await fetchPhotos(trimInput, page);
+    const photosArr = varPhotos.hits;
 
-    } catch (error) {
-        console.log('ups');
-    };
-})
+    if (varPhotos.hits.length === 0) {
+      throw new Error();
+    }
+    console.log(photosArr);
+    console.log(trimInput);
+  } catch (error) {
+    console.log('ups');
+  }
+});
 
 
 // const totalHits = photos.totalHits;
